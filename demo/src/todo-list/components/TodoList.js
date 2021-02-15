f4a12a63b08a5dfab4394b132841e56339bc531b
Vladimir Gorej <vladimir.gorej@gmail.com>
2021-02-15 20:29:11 +0100

    refactor(demo): remove unused imports

--- demo/src/todo-list/components/TodoList.js
+++ demo/src/todo-list/components/TodoList.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
 import Divider from '@material-ui/core/Divider';
 import { makeStyles } from '@material-ui/core/styles';
 import { useSystemComponent, useSystemSelector } from 'swagger-adjust';
